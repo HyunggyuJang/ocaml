708279488fe9b09b6eb76f6a27aa7fd79b14b77a
Hyunggyu Jang <murasakipurplez5@gmail.com>
2022-08-03 19:42:19 +0900

    Update test

--- testsuite/tests/tool-ocamltest/parsing.ml
+++ testsuite/tests/tool-ocamltest/parsing.ml
@@ -11,6 +11,18 @@ open Tsl_semantics
 open Tsl_ast
 let block = tsl_block token (Lexing.from_string {xx|
 (* TEST
+   ld_library_path += "${ocamlsrcdir}/ocamltest ${ocamlsrcdir}/otherlibs/unix"
+   flags = "-I ${ocamlsrcdir}/ocamltest"
+   * expect
+*)
+
+#load "ocamltest.cma";;
+open Tsl_parser
+open Tsl_lexer
+open Tsl_semantics
+open Tsl_ast
+let block = tsl_block token (Lexing.from_string {x|
+(* TEST
    flags = "-I ${ocamlsrcdir}/ocamltest"
    * expect
 *)
@@ -33,7 +45,7 @@ Tsl_parser.tsl_block Tsl_lexer.token (Lexing.from_string {|
    loc = <abstr>};
  Tsl_ast.Test (1, {Tsl_ast.node = "expect"; loc = <abstr>}, [])]
 |}];;
-|xx});;
+|x});;
 test_trees_of_tsl_block block;;
 [%%expect {|
 val block : Tsl_ast.tsl_block =
@@ -47,6 +59,39 @@ val block : Tsl_ast.tsl_block =
     Tsl_semantics.test_tree list
 =
 ([{node =
+    Assignment (false, {node = "flags"; loc = <abstr>},
+     {node = "-I ${ocamlsrcdir}/ocamltest"; loc = <abstr>});
+   loc = <abstr>}],
+ [Node ([],
+   {Tests.test_name = "expect"; test_run_by_default = false;
+    test_actions = [<abstr>; <abstr>; <abstr>]},
+   [], [])])
+|}];;
+|xx});;
+test_trees_of_tsl_block block;;
+[%%expect {|
+val block : Tsl_ast.tsl_block =
+  [Environment_statement
+    {node =
+      Append ({node = "ld_library_path"; loc = <abstr>},
+       {node = "${ocamlsrcdir}/ocamltest ${ocamlsrcdir}/otherlibs/unix";
+        loc = <abstr>});
+     loc = <abstr>};
+   Environment_statement
+    {node =
+      Assignment (false, {node = "flags"; loc = <abstr>},
+       {node = "-I ${ocamlsrcdir}/ocamltest"; loc = <abstr>});
+     loc = <abstr>};
+   Test (1, {node = "expect"; loc = <abstr>}, [])]
+- : Tsl_ast.environment_statement Tsl_ast.located list *
+    Tsl_semantics.test_tree list
+=
+([{node =
+    Append ({node = "ld_library_path"; loc = <abstr>},
+     {node = "${ocamlsrcdir}/ocamltest ${ocamlsrcdir}/otherlibs/unix";
+      loc = <abstr>});
+   loc = <abstr>};
+  {node =
     Assignment (false, {node = "flags"; loc = <abstr>},
      {node = "-I ${ocamlsrcdir}/ocamltest"; loc = <abstr>});
    loc = <abstr>}],
